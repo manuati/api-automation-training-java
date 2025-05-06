5aabf6c5015bbb83036467914ba074be2e4ed241
manuati <manuatilomb@gmail.com>
2025-05-06 16:37:41 -0300

    Removed example service

---
+++ framework/src/test/java/services/BookingServiceTests.java
@@ -0,0 +1,12 @@
+package services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BookingServiceTests {
+
+    @Test
+    public void dummyTest() {
+        Assertions.assertTrue(true);
+    }
+}
